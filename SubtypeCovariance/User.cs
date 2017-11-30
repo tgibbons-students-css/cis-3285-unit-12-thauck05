592d8936bb65fdcdde5a395ed4748bf45ae97840
thauck <thauck@css.edu>
2017-11-29 21:31:25 -0600

    Completed Unit 12 Assignment.

--- SubtypeCovariance/User.cs
+++ SubtypeCovariance/User.cs
@@ -8,6 +8,11 @@ namespace SubtypeCovariance
 {
     public class User : Entity
     {
+        public User(Guid newID) : base(newID)
+        {
+            EmailAddress = "";
+        }
+
         public string EmailAddress { get; set; }
 
         public DateTime DateOfBirth { get; set; }
